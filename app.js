90319b85677d40af946be2149f65a0b9658342bf
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2023-12-01 16:03:58 -0300

    ALL
    ALL

---
+++ app.js
@@ -0,0 +1,15 @@
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// import express from "express";
+// let app = express();
+
+// app.use(express.static("public"));
+// // respond with "hello world" when a GET request is made to the homepage
+// app.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "/index.html"));
+// });
+
+// var server = app.listen(3000);
